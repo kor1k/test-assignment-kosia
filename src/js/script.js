2a21b44d37a8214fbea153f258111ba3b17e8217
kor1k <koortttt@gmail.com>
2019-04-20 15:21:57 +0300

    added some example cards

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,26 @@
 jQuery(document).ready(function ($) {
     $('.section-main-content-slider-items').slick({
         infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 4,
+        slidesToScroll: 4,
         dots: true,
+        dotsClass: "my-dots",
         autoplay: true,
         autoplaySpeed: 2000,
+        responsive: [{
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+            {
+                breakpoint: 400,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
     });
 });
